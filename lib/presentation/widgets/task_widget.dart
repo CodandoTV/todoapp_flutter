90bf72f69c1a48388f44c66c4bb71fc31c2199e2
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-03-10 07:46:21 -0300

    Update task status use case

---
+++ lib/presentation/widgets/task_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TaskWidget extends StatelessWidget {
+  final String title;
+  final String? desc;
+  final bool isChecked;
+  final Function(bool?) onCheckChanged;
+
+  const TaskWidget({
+    super.key,
+    required this.title,
+    required this.desc,
+    required this.isChecked,
+    required this.onCheckChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      title: Text(
+        title,
+      ),
+      subtitle: Text(
+        desc ?? "",
+      ),
+      trailing: Checkbox(
+        value: isChecked,
+        onChanged: onCheckChanged,
+      ),
+    ));
+  }
+}
