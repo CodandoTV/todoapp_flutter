78c3c7b14555c7a6c0226a69c5c3f205f14d5a0e
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-03-11 09:41:18 -0300

    Add new use case to get categories

--- lib/domain/usecases/get_categories_usecase.dart
+++ lib/domain/usecases/get_categories_usecase.dart
@@ -2,6 +2,16 @@ import 'package:todo_app/domain/model/task_type.dart';
 
 class GetCategoriesUseCase {
   List<String> get() {
-    return TaskType.values.map((element) => element.name).toList();
+    var tasks = TaskType.values.toList();
+    tasks.sort(_compare);
+    return tasks.map((toElement) => toElement.name).toList();
+  }
+
+  int _compare(TaskType a, TaskType b) {
+    if (a == TaskType.unknown) {
+      return -1;
+    } else {
+      return 1;
+    }
   }
 }
