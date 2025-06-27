b678c182b11290853b317277945a9c58ec55cb58
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-06-27 18:48:51 -0300

    Update application id + namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.todoapp"
+    namespace = "com.gabrielbmoro.todoapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.todoapp"
+        applicationId = "com.gabrielbmoro.todoapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
