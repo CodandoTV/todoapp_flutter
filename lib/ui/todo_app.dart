6de139f1efdf88988450d876ab7ef715b82f27eb
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-07-18 15:42:23 -0300

    Fix import paths for AppLocalizations slash.

--- lib/ui/todo_app.dart
+++ lib/ui/todo_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/ui/l10n//app_localizations.dart';
+import 'package:todoapp/ui/l10n/app_localizations.dart';
 import 'package:todoapp/ui/todo_app_router_config.dart';
 
 class TodoApp extends StatelessWidget {
