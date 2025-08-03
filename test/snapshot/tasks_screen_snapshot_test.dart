3838db99ff30404aba3d0d6b83f838f322c702eb
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-08-03 20:40:41 -0300

    Add snapshot tests for TasksScreen with 50% and 100% progress
    This commit introduces two new snapshot tests for the `TasksScreen`:
    - `TasksScreen - Snapshot - Some tasks with progress 50%`: Verifies the UI when there are tasks and the progress is at 50%.
    - `TasksScreen - Snapshot - Some tasks with progress 100%`: Verifies the UI when all tasks are completed and the progress is at 100%.
    
    Golden files for these scenarios have also been added.

--- test/snapshot/tasks_screen_snapshot_test.dart
+++ test/snapshot/tasks_screen_snapshot_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todoapp/data/model/task.dart';
 import 'package:todoapp/ui/screens/tasks/tasks_screen.dart';
 import 'package:todoapp/ui/screens/tasks/tasks_screen_state.dart';
 import 'package:todoapp/ui/screens/tasks/tasks_screen_text_values.dart';
@@ -44,6 +45,98 @@ void main() {
         find.byType(TasksScaffold),
         matchesGoldenFile(
           'goldens/tasks_screen_empty_state_snapshot.png',
+        ),
+      );
+    },
+  );
+
+  testWidgets(
+    'TasksScreen - Snapshot - Some tasks with progress 50%',
+        (tester) async {
+      final widget = WidgetsUtil.buildMaterialAppWidgetTest(
+        child: TasksScaffold(
+          navigatorProvider: FakeNavigatorProvider(),
+          uiState: const TasksScreenState(
+            tasks: [
+              Task(id: 1, title: 'task1', isCompleted: false),
+              Task(id: 2, title: 'task2', isCompleted: true),
+              Task(id: 3, title: 'task3', isCompleted: false),
+              Task(id: 4, title: 'task4', isCompleted: true),
+            ],
+            isLoading: false,
+            progress: 0.5,
+            showShareIcon: true,
+          ),
+          updateTasks: () => {},
+          onShare: () => {},
+          onCompleteTask: (_, __) => {},
+          onRemoveTask: (_) => {},
+          onReorder: (_, __) => {},
+          checklistId: 1,
+          checklistName: 'pets',
+          tasksScreenTextValues: const TasksScreenTextValues(
+            taskAdded: 'taskAdded',
+            removeTaskDialogTitle: 'removeTaskDialogTitle',
+            removeTaskDialogDesc: 'removeTaskDialogDesc',
+            yes: 'yes',
+            no: 'no',
+            emptyTasksMessage: 'emptyTasksMessage',
+          ),
+        ),
+        tester: tester,
+      );
+
+      await tester.pumpWidget(widget);
+
+      await expectLater(
+        find.byType(TasksScaffold),
+        matchesGoldenFile(
+          'goldens/tasks_screen_fifty_percent_state_snapshot.png',
+        ),
+      );
+    },
+  );
+
+  testWidgets(
+    'TasksScreen - Snapshot - Some tasks with progress 100%',
+        (tester) async {
+      final widget = WidgetsUtil.buildMaterialAppWidgetTest(
+        child: TasksScaffold(
+          navigatorProvider: FakeNavigatorProvider(),
+          uiState: const TasksScreenState(
+            tasks: [
+              Task(id: 2, title: 'task2', isCompleted: true),
+              Task(id: 4, title: 'task4', isCompleted: true),
+            ],
+            isLoading: false,
+            progress: 1,
+            showShareIcon: true,
+          ),
+          updateTasks: () => {},
+          onShare: () => {},
+          onCompleteTask: (_, __) => {},
+          onRemoveTask: (_) => {},
+          onReorder: (_, __) => {},
+          checklistId: 1,
+          checklistName: 'pets',
+          tasksScreenTextValues: const TasksScreenTextValues(
+            taskAdded: 'taskAdded',
+            removeTaskDialogTitle: 'removeTaskDialogTitle',
+            removeTaskDialogDesc: 'removeTaskDialogDesc',
+            yes: 'yes',
+            no: 'no',
+            emptyTasksMessage: 'emptyTasksMessage',
+          ),
+        ),
+        tester: tester,
+      );
+
+      await tester.pumpWidget(widget);
+
+      await expectLater(
+        find.byType(TasksScaffold),
+        matchesGoldenFile(
+          'goldens/tasks_screen_one_hundred_percent_state_snapshot.png',
         ),
       );
     },
