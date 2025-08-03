6356133936b5543ba215276be6358cc1790ba62a
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-08-03 19:46:32 -0300

    Add ChecklistsScreen snapshot test with multiple checklists

--- test/snapshot/checklists_screen_snapshot_test.dart
+++ test/snapshot/checklists_screen_snapshot_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todoapp/data/model/checklist.dart';
 import 'package:todoapp/ui/screens/checklists/checklists_screen.dart';
 import 'package:todoapp/ui/screens/checklists/checklists_screen_state.dart';
 import 'package:todoapp/ui/screens/checklists/checklists_screen_text_values.dart';
@@ -38,6 +39,47 @@ void main() {
         find.byType(ChecklistsScaffold),
         matchesGoldenFile(
           'goldens/checklists_screen_empty_state_snapshot.png',
+        ),
+      );
+    },
+  );
+
+  testWidgets(
+    'ChecklistsScreen - Snapshot - Some checklists',
+        (tester) async {
+      final widget = WidgetsUtil.buildMaterialAppWidgetTest(
+        child: ChecklistsScaffold(
+          uiState: const ChecklistsScreenState(
+            checklists: [
+              Checklist(id: null, title: 'pets'),
+              Checklist(id: null, title: 'supermarket'),
+              Checklist(id: null, title: 'drugstore'),
+              Checklist(id: null, title: 'street market'),
+            ],
+            isLoading: false,
+          ),
+          checklistsScreenTextValues: const ChecklistsScreenTextValues(
+            screenTitle: 'checklists',
+            checklistAdded: 'A new checklist has been added',
+            removeChecklistDialogTitle: 'Are you sure you want to remove it?',
+            removeChecklistDialogDesc: 'Remove this checklist',
+            yes: 'yes',
+            no: 'no',
+            emptyChecklistMessage: 'You have no checklists',
+          ),
+          onRemoveChecklist: (_) => {},
+          navigatorProvider: FakeNavigatorProvider(),
+          updateChecklists: () => {},
+        ),
+        tester: tester,
+      );
+
+      await tester.pumpWidget(widget);
+
+      await expectLater(
+        find.byType(ChecklistsScaffold),
+        matchesGoldenFile(
+          'goldens/checklists_screen_some_checklists_snapshot.png',
         ),
       );
     },
