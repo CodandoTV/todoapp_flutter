53bbb0aca85838295a6a11d29644544b3f1798ab
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-12-14 21:55:18 -0300

    Add blur effect and auto-close - Expandable fab menu

--- lib/ui/components/widgets/checklist/fabmenu/checklist_expandable_fab_menu.dart
+++ lib/ui/components/widgets/checklist/fabmenu/checklist_expandable_fab_menu.dart
@@ -3,7 +3,7 @@ import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:todoapp/ui/components/widgets/checklist/fabmenu/checklist_fabmenu_item.dart';
 import 'package:todoapp/ui/l10n/app_localizations.dart';
 
-class ChecklistExpandableFabMenu extends StatelessWidget {
+class ChecklistExpandableFabMenu extends StatefulWidget {
   final Function() onNewChecklistPressed;
   final Function() onNewTaskPressed;
 
@@ -14,21 +14,40 @@ class ChecklistExpandableFabMenu extends StatelessWidget {
   });
 
   @override
+  State<ChecklistExpandableFabMenu> createState() =>
+      _ChecklistExpandableFabMenuState();
+}
+
+class _ChecklistExpandableFabMenuState
+    extends State<ChecklistExpandableFabMenu> {
+  final _key = GlobalKey<ExpandableFabState>();
+
+  @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
 
     return ExpandableFab(
+      key: _key,
       type: ExpandableFabType.side,
+      overlayStyle: const ExpandableFabOverlayStyle(
+        blur: 1.0,
+      ),
       children: [
         ChecklistFabMenuItem(
           heroTag: 'new_task',
-          onPressed: onNewTaskPressed,
+          onPressed: () => {
+            _key.currentState?.close(),
+            widget.onNewTaskPressed(),
+          },
           label: localizations.task,
           icon: Icons.add_task,
         ),
         ChecklistFabMenuItem(
           heroTag: 'new_checklist',
-          onPressed: onNewChecklistPressed,
+          onPressed: () => {
+            _key.currentState?.close(),
+            widget.onNewChecklistPressed(),
+          },
           label: localizations.checklist,
           icon: Icons.plus_one,
         ),
